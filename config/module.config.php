cc541e5afa6af0acae87144eed311fdc5ea4f7e5
Tomas Kormanak <kormik@kormik.net>
2012-11-22 14:36:44 +0100

    sandbox

--- config/module.config.php
+++ config/module.config.php
@@ -1,9 +1,20 @@
 <?php
 /**
- * SampleModule configuration file
+ * Sandbox configuration file
  *
  */
 return array(
     'vivo'      => array(
+        'templates' => array (
+            'templateMap' => array(
+//                 'Vivo\UI\Page' => __DIR__.'/../view/Vivo/UI/Page.phtml',
+//                 'Vivo\CMS\UI\Content\Sample' => __DIR__.'/../view/Vivo/CMS/UI/Content/Sample.phtml',
+//                 'Vivo\CMS\UI\Content\Layout' => __DIR__.'/../view/Vivo/CMS/UI/Content/Layout.phtml',
+//                 'Vivo\CMS\UI\Content\File:html' => __DIR__.'/../view/Vivo/CMS/UI/Content/File.html.phtml',
+//                 'Vivo\CMS\UI\Content\File:plain' => __DIR__.'/../view/Vivo/CMS/UI/Content/File.plain.phtml',
+//                 'Vivo\CMS\UI\Content\File:flash' => __DIR__.'/../view/Vivo/CMS/UI/Content/File.flash.phtml',
+//                 'Vivo\CMS\UI\Content\File' => __DIR__.'/../view/Vivo/CMS/UI/Content/File.phtml',
+            ),
+        ),
     ),
 );
